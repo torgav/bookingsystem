0482b299b060153e25198f1689a459af2d2eaaed
Christian Torres <christian.torresgaviria@elev.ga.ntig.se>
2023-03-22 14:57:02 +0100

    Har byggt up min main struktur allstå så har jag skapat kärnan av allt.

--- src/bookingsystem/Bookingsystem.java
+++ src/bookingsystem/Bookingsystem.java
@@ -1,8 +1,55 @@
 package bookingsystem;
 
+import java.util.Scanner;
+
 public class Bookingsystem {
-    public static void main(String[] args) {
+    //GLOBAL VARIABLES
+       static Scanner sc = new Scanner(System.in);
+       static String userInput = "";
        
+       
+    public static void main(String[] args) {
+        int convert = 0;
+        while(true){
+            try{
+                System.out.println("MENU:");
+                System.out.println("{1] BOOK SEAT");
+                System.out.println("[2] AVAILABLE SEATS");
+                System.out.println("[3] PROFIT");
+                System.out.println("[4] END PROGRAM");
+                System.out.println("INPUT:");
+                userInput = sc.nextLine();
+                convert = Integer.parseInt(userInput);
+            }catch(Exception e){
+                System.out.println("INVALID INPUT");
+            }
+            switch(convert){
+                case 1:
+                    bookSeat();
+                case 2:
+                    availableSeat();
+                case 3:
+                    profit();
+                case 4:
+                    System.out.println("Exiting program...");
+                    System.exit(0);
+                default:
+                    System.out.println("INAVLID INPUT");
+                    continue;
+            }           
+        }
     }
+    
+    static void bookSeat(){
+    
+    }
+    
+    static void availableSeat(){
+    
+    }
+    
+    static void profit(){
+    
+ }
     
 }
