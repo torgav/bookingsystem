ac2b9259232061addd3ca81d65b283e2b4eb1ee7
Christian Torres <christian.torresgaviria@elev.ga.ntig.se>
2023-03-22 23:40:55 +0100

    Gjort klar min main där allting kopplas till metoder. Gör nu första metoden om att boka platser.

--- src/bookingsystem/Bookingsystem.java
+++ src/bookingsystem/Bookingsystem.java
@@ -7,7 +7,6 @@ public class Bookingsystem {
        static Scanner sc = new Scanner(System.in);
        static String userInput = "";
        
-       
     public static void main(String[] args) {
         int convert = 0;
         while(true){
@@ -17,7 +16,7 @@ public class Bookingsystem {
                 System.out.println("[2] AVAILABLE SEATS");
                 System.out.println("[3] PROFIT");
                 System.out.println("[4] END PROGRAM");
-                System.out.println("INPUT:");
+                System.out.print("INPUT: ");
                 userInput = sc.nextLine();
                 convert = Integer.parseInt(userInput);
             }catch(Exception e){
@@ -26,10 +25,13 @@ public class Bookingsystem {
             switch(convert){
                 case 1:
                     bookSeat();
+                    continue;
                 case 2:
                     availableSeat();
+                    continue;
                 case 3:
                     profit();
+                    continue;
                 case 4:
                     System.out.println("Exiting program...");
                     System.exit(0);
@@ -41,7 +43,37 @@ public class Bookingsystem {
     }
     
     static void bookSeat(){
-    
+        int[] seatPlaces = new int[22];
+        int socialNumber = 0;
+        
+        while(true){
+            try{
+                System.out.print("ENTER YOUR SOCIAL NUMBER [YYMMDD]: ");
+                socialNumber = sc.nextInt();
+            }catch(Exception e){
+                System.out.println("INVALID INPUT");
+                continue;
+            }
+            if(socialNumber < 000101 || socialNumber >991231){
+                System.out.println("INVALID INPUT");
+                continue;
+            }
+            break;
+        }
+        
+        System.out.println(seatPlaces[1]);
+        for(int i = 1;i < seatPlaces.length;i++){
+            
+            if(seatPlaces[i] == 0){
+                seatPlaces[i] = socialNumber;
+                System.out.println("YOUR SEAT NUMBER IS: "+i);
+                break;
+            }
+        }
+        System.out.print("PRESS ENTER TO RETURN");
+        sc.nextLine();
+        sc.nextLine();
+        
     }
     
     static void availableSeat(){
